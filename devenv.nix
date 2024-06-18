6484d7b1f38f56abc167e4c0082c00d1023f221d
Ben Lovell <ben.j.lovell@gmail.com>
2024-06-18 21:36:15 +0200

    Devenv improvement

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
   # https://devenv.sh/reference/options/
-  packages = [ pkgs.figlet pkgs.lolcat pkgs.babashka ];
+  packages = [ pkgs.figlet pkgs.lolcat pkgs.babashka pkgs.cljfmt];
   languages.clojure.enable = true;
 
   enterShell = ''
-    figlet -f small -k "Nix shellified!" | lolcat -F 0.5 -ad 1 -s 30
+    figlet -f small -k "Kahawa Devenv" | lolcat -F 0.5 -ad 1 -s 30
   '';
 
   # Run an arbitrary program on start
